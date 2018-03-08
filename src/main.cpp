70a89411ab59283c77029f3eaffa2b0efc33eb19
Omar Aflak <aflakomar@gmail.com>
2018-03-08 11:47:49 +0100

    catch read failure in main

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,16 @@
 int main(int argc, char const *argv[]) {
 	std::vector<std::vector<Node> > nodes;
 	std::vector<std::string> labyrinth;
-	if(read_labyrinth("data/labyrinth3", nodes, labyrinth)){
+	const char* filepath = "./data/labyrinth3";
+
+	if(read_labyrinth(filepath, nodes, labyrinth)){
 		BFS(nodes, 0, 0);
 		print(nodes, labyrinth, std::cout);
 		free_memory(nodes);
+	}
+	else{
+		std::cerr << "Could not read " << filepath << std::endl;
+		return 1;
 	}
 	return 0;
 }
